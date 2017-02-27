885cf1b224a296c0cdb9449b0095f9c3a6d8940c
agauchy <antho2gauchy@gmail.com>
2017-02-27 16:24:57 +0100

    Tentative historique, cursor ok, retour arriere ok.

--- ia32/main.c
+++ ia32/main.c
@@ -21,8 +21,8 @@ typedef unsigned short uint16_t;
 #define COM1 ((uint16_t)0x3f8)
 #define COM2 ((uint16_t)0x2f8)
 
-static __inline        __attribute__((always_inline, no_instrument_function))
-       uint8_t inb(
+static __inline                     __attribute__((always_inline, no_instrument_function))
+                    uint8_t inb(
 		uint16_t port) {
 	uint8_t data;
 	__asm __volatile("inb %w1,%0" : "=a" (data) : "d" (port));
@@ -108,7 +108,15 @@ volatile char* write_char_end(int colour, char c, volatile char* position) {
 	return position;
 }
 
-int i = 0;
+volatile void write_return(volatile char* position, int linePosition) {
+	position -= 2;
+	int i = 0;
+	for (i = linePosition * 2; i < 158; i++) {
+		*position++ = *(position + 1);
+	}
+	*position = ' ';
+}
+
 void kputchar(int c, void *arg) {
 	serial_write_char(COM2, c);
 }
@@ -124,22 +132,26 @@ void make_screen_scroll() {
 
 }
 
-void update_cursor(int row, int col)
- {
-    unsigned short position=(row*80) + col;
-    // cursor LOW port to vga INDEX register
-    outb(0x3D4, 0x0F);
-    outb(0x3D5, (unsigned char)(position&0xFF));
-    // cursor HIGH port to vga INDEX register
-    outb(0x3D4, 0x0E);
-    outb(0x3D5, (unsigned char )((position>>8)&0xFF));
- }
+void update_cursor(int row, int col) {
+	unsigned short position = (row * 80) + col;
+	// cursor LOW port to vga INDEX register
+	outb(0x3D4, 0x0F);
+	outb(0x3D5, (unsigned char) (position & 0xFF));
+	// cursor HIGH port to vga INDEX register
+	outb(0x3D4, 0x0E);
+	outb(0x3D5, (unsigned char) ((position >> 8) & 0xFF));
+}
 
 void kmain(void) {
 	volatile char *positionQemu = (volatile char*) 0xB8000;
 	int lineSize = 0;
 	int maxLineSize = 0;
 	int currentLine = 0;
+
+	char history[50][79];
+	int lineSizeInHistory[50];
+	int currentLineInHistory = 0;
+	int totalLineInHistory = 0;
 
 	write_string(0x2a, "Console greetings!");
 	serial_init(COM1);
@@ -148,10 +160,11 @@ void kmain(void) {
 
 	serial_write_char(COM1, '>');
 	positionQemu = write_string_end(0x2a, ">", positionQemu);
+	update_cursor(currentLine, lineSize + 1);
 	while (1) {
 		unsigned char c;
 		c = serial_read(COM1);
-		if (c == 13) {
+		if (c == 13) { // touche entrée
 			c = '\r';
 			serial_write_char(COM1, c);
 			c = '\n';
@@ -164,11 +177,24 @@ void kmain(void) {
 				positionQemu -= 2 * (lineSize + 1);
 				positionQemu = write_string_end(0x2a, ">", positionQemu);
 				lineSize = 0;
+				maxLineSize = 0;
 			} else {
 				positionQemu += 2 * (79 - lineSize);
 				positionQemu = write_string_end(0x2a, ">", positionQemu);
 				lineSize = 0;
+				maxLineSize = 0;
 			}
+			if (totalLineInHistory < 50) {
+				totalLineInHistory++;
+			} else {
+				int i = 0;
+				for (i = 0; i < 50; i++) {
+					//history[i] = history[i+1];
+				}
+			}
+			currentLineInHistory = totalLineInHistory;
+			update_cursor(currentLine > 24 ? 24 : currentLine,
+					lineSize == 79 ? lineSize : lineSize + 1);
 		} else if (c == 27) { //Flèches code 1
 			c = serial_read(COM1);
 			if (c == 91) { //Flèches code 2
@@ -177,25 +203,56 @@ void kmain(void) {
 					if (lineSize) {
 						positionQemu -= 2;
 						lineSize--;
+						update_cursor(currentLine > 24 ? 24 : currentLine,
+								lineSize == 79 ? lineSize : lineSize + 1);
 					}
 				} else if (c == 65) { //Flèche du haut
-
+					if (currentLineInHistory > 0) {
+						positionQemu = write_string_end(0x2a,
+								history[--currentLineInHistory],
+								positionQemu - 2 * lineSize);
+						lineSize = lineSizeInHistory[currentLineInHistory];
+						update_cursor(currentLine > 24 ? 24 : currentLine,
+								lineSize == 79 ? lineSize : lineSize + 1);
+					}
 				} else if (c == 66) { //Flèche du bas
-
+					if (currentLineInHistory < totalLineInHistory) {
+						positionQemu = write_string_end(0x2a,
+								history[++currentLineInHistory],
+								positionQemu - 2 * lineSize);
+						lineSize = lineSizeInHistory[currentLineInHistory];
+						update_cursor(currentLine > 24 ? 24 : currentLine,
+								lineSize == 79 ? lineSize : lineSize + 1);
+					}
 				} else if (c == 67) { //Flèche de droite
 					if (lineSize < maxLineSize) {
 						positionQemu += 2;
 						lineSize++;
+						update_cursor(currentLine,
+								lineSize == 79 ? lineSize : lineSize + 1);
 					}
 				}
+			}
+		} else if (c == 127) {
+			if (lineSize > 0) {
+				write_return(positionQemu, lineSize);
+				positionQemu -= 2;
+				lineSize--;
+				history[currentLineInHistory][lineSize] = ' ';
+				update_cursor(currentLine > 24 ? 24 : currentLine,
+						lineSize + 1);
 			}
 		} else if (lineSize < 79) { //Si il y a toujours de la place sur la ligne
 			serial_write_char(COM1, c);
 			positionQemu = write_char_end(0x2a, c, positionQemu);
+			history[currentLineInHistory][lineSize] = c;
+			lineSizeInHistory[currentLineInHistory]++;
 			lineSize++;
 			if (lineSize > maxLineSize) {
 				maxLineSize = lineSize;
 			}
+			update_cursor(currentLine > 24 ? 24 : currentLine,
+					lineSize == 79 ? lineSize : lineSize + 1);
 		}
 	}
 
